155a78725adfdeddea072fbb8abb584455e2f4c9
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-30 11:13:24 +0200

    I have created a header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import React from "react"
+import Header from "./component/Header"
 
 function App() {
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+       <Header />
+    </div>
+)
 }
 
 export default App
