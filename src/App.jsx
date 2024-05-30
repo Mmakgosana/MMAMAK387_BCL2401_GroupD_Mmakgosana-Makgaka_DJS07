9e4e0460eadd1fea9731cf0b9642b81f12bffb49
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-30 15:35:10 +0200

    I have corrected the syntax

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react"
 import Header from "./component/Header"
 import Meme from "./component/Meme"
 
+
 export default function App() {
 
   return (
-    <>
     <div>
        <Header />
        <Meme />
     </div>
-    </>
+  
     
-    )
+  )
 }
 
